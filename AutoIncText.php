32dad7377ca6c3a80da550c913e8c5a8b9c02dae
Enrique Nieto Martínez <46055712+proartmateur@users.noreply.github.com>
2020-10-01 18:12:29 -0500

    Fix Index Out Of range

--- AutoIncText.php
+++ AutoIncText.php
@@ -1,19 +1,22 @@
 <?php
-
 class AutoIncText
 {
     private $start = 0;
     private $historia = array();
     private $prefix = "";
     private $arr = array();
-
+    private $max_key_val = 0;
+    private $first = 0;
+    
     public function __construct($start_int = 7, $name_prefix = "TEXTO_", $texto_arr)
     {
         print "En el constructor AutoIncText\n";
         $this->start = $start_int;
+        $this->first = $start_int;
         $this->prefix = $name_prefix;
         $this->arr = $texto_arr;
-
+        $this->max_key_val = count($this->arr) + $this->first;
+        
         $this->inicializar();
     }
 
@@ -40,7 +43,13 @@ class AutoIncText
 
     public function texto($name_of_val)
     {
-        return $this->historia[$name_of_val];
+        $find_index = intval(str_replace($this->prefix, "", $name_of_val));
+        $is_in_range = ($this->max_key_val > $find_index) && ($find_index >= $this->first);
+        
+        if($is_in_range) {
+            return $this->historia[$name_of_val];    
+        }
+        return "NOT VAL";
     }
     public function print_historia()
     {
